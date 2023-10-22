92e2ee193e2f5411633845b5540901185c6147ad
Boyoung <boyoung.y589@gmail.com>
2023-10-23 05:48:32 +0900

    add: header에 배열형태로 컴포넌트를 props로 내려주기

--- learn-nextjs/components/common/Header.tsx
+++ learn-nextjs/components/common/Header.tsx
@@ -1,15 +1,27 @@
 import Link from "next/link";
 import styles from '../../styles/header.module.scss';
+import Image from "next/image";
+import InflearnImg from "/public/inflearn.png";
 
-interface Props {}
-const HeaderComponent = ({ }: Props) => {
+interface Props {
+  rightElements?: React.ReactElement[];
+}
+const HeaderComponent = ({rightElements}: Props) => {
   return (
     <header className={styles.header}>
       <div className={styles.flexItem}>
         <Link href="/" className={styles.box}>
-          <img src='/inflearn.png' width={110} height={20} alt="인프런 로고" />
+          <Image
+            src={InflearnImg}
+            width={110}
+            height={20}
+            alt="인프런 로고" 
+            />
         </Link>
       </div>
+      {rightElements && <div className={styles.flexItem}>
+        {rightElements}
+      </div>}
     </header>
   );
 }
--- learn-nextjs/pages/index.tsx
+++ learn-nextjs/pages/index.tsx
@@ -1,12 +1,36 @@
-import Header from "../components/common/Header";
 import { Fragment } from "react";
+import Header from "../components/common/Header";
+import styles from '../styles/header.module.scss';
+import Link from "next/link";
+import { VscFeedback } from 'react-icons/vsc';
+import { AiOutlineShareAlt } from 'react-icons/ai';
 
 export default function Home() {
   return (
     <Fragment>
-      <Header />
-      <main></main>
+      <Header
+        rightElements={[
+          <button
+            onClick={() => {
+              alert('복사!')
+            }}
+            className={styles.box}
+            style={{ marginRight: 8 }}
+            key="button"
+          >
+            <AiOutlineShareAlt size={20} />
+          </button>,
+          <Link
+            href="/feedback"
+            className={styles.box}
+            key="link"
+          >
+            <VscFeedback size={20} />
+          </Link>,
+        ]}
+      />
+      <main style={{ width: '100%', height: '100%' }}>
+      </main>
     </Fragment>
-    
   );
 }
